8edfae9af8fcddda0f5afe3ee66e611eb6449e17
[dyad] <funilsites@gmail.com>
2025-09-22 20:17:20 -0300

    Reverted all changes back to version 227f14b46effd0ec9f11a388b25d0728489cfd42

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,9 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Admin from "./pages/Admin";
-import Aluno from "./pages/Aluno";
-import Perfil from "./pages/Perfil";
 
 const queryClient = new QueryClient();
 
@@ -16,15 +13,13 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/aluno" element={<Aluno />} />
-          <Route path="/perfil" element={<Perfil />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
-import { BannerProvider } from "@/context/BannerContext";
-import { ModulosProvider } from "@/context/ModulosContext";
-import { LogoProvider } from "@/context/LogoContext";
-import { PhotoProvider } from "@/context/PhotoContext";
-import { UserProvider } from "@/context/UserContext";
 
-createRoot(document.getElementById("root")!).render(
-  <UserProvider>
-    <LogoProvider>
-      <BannerProvider>
-        <ModulosProvider>
-          <PhotoProvider>
-            <App />
-          </PhotoProvider>
-        </ModulosProvider>
-      </BannerProvider>
-    </LogoProvider>
-  </UserProvider>
-);
+createRoot(document.getElementById("root")!).render(<App />);
