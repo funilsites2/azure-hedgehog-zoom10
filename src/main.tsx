c73880f5ebf20502399b20ca47f22d379eec76af
[dyad] <funilsites@gmail.com>
2025-09-21 23:38:46 -0300

    Reverted all changes back to version 9f10f27d7a9966745ed0ed6a70f17966f63d9764

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,11 @@ import App from "./App.tsx";
 import "./globals.css";
 import { BannerProvider } from "@/context/BannerContext";
 import { ModulosProvider } from "@/context/ModulosContext";
-import { StudentSettingsProvider } from "@/context/StudentSettingsContext";
 
 createRoot(document.getElementById("root")!).render(
   <BannerProvider>
     <ModulosProvider>
-      <StudentSettingsProvider>
-        <App />
-      </StudentSettingsProvider>
+      <App />
     </ModulosProvider>
   </BannerProvider>
 );
