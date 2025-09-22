e270c134107c0bff05b5b171c9e07d8386ae0a1d
[dyad] <funilsites@gmail.com>
2025-09-21 22:24:28 -0300

    [dyad] Move ModulosProvider para main.tsx e remove wrapper duplicado em App.tsx - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Admin from "./pages/Admin";
 import Aluno from "./pages/Aluno";
-import { ModulosProvider } from "@/context/ModulosContext";
 
 const queryClient = new QueryClient();
 
@@ -16,17 +15,15 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <ModulosProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/admin" element={<Admin />} />
-            <Route path="/aluno" element={<Aluno />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </ModulosProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/aluno" element={<Aluno />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
+import { ModulosProvider } from "@/context/ModulosContext";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <ModulosProvider>
+    <App />
+  </ModulosProvider>
+);
